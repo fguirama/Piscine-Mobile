c8518de2f7c16029214f065324e0e96361fda7f6
fguirama <florian.guiramand@protonmail.com>
2026-07-18 20:34:40 +0200

    fix module 0

--- 0-Basic-of-the-mobile-application/mobileModule00/ex00/src/app/index.tsx
+++ 0-Basic-of-the-mobile-application/mobileModule00/ex00/src/app/index.tsx
@@ -4,7 +4,7 @@ export default function HomeScreen() {
     return (<View className="flex-1 items-center justify-center gap-3">
         <Text className="text-xl rounded-lg bg-green-500 px-5 py-3">A simple text</Text>
         <Pressable className="rounded-full bg-white px-5 py-3">
-            <Text className="text-green-500" onPress={() => console.log("Button pressed”")}>Click me</Text>
+            <Text className="text-green-500" onPress={() => console.log("Button pressed")}>Click me</Text>
         </Pressable>
     </View>);
 }
