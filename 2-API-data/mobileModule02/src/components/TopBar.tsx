8e1201a7c21a577936453d98b1cf271a6da08655
florian <florian.guiramand@protonmail.com>
2026-07-20 20:00:42 +0200

    fix: search display "not found"

--- 2-API-data/mobileModule02/src/components/TopBar.tsx
+++ 2-API-data/mobileModule02/src/components/TopBar.tsx
@@ -10,9 +10,10 @@ import {useEffect, useRef} from "react";
 export default function TopBar() {
     const {search, setSearch, setSearchError, setError, searchResult, setSearchResult} = useSearch();
     const {setWeather} = useWeather();
-    const iptRef = useRef(null);
+    const iptRef = useRef<TextInput>(null);
 
     const makeWeatherRequest = async (latitude: number, longitude: number, location: iLocation) => {
+        setSearchResult(undefined);
         try {
             const requestRes = await getWeather(latitude, longitude);
             setWeather([requestRes, location]);
@@ -56,20 +57,18 @@ export default function TopBar() {
     const handleChangeText = async (text: string) => {
         setSearch(text);
         setError(false);
-        try {
-            const requestRes = await getGeocoding(text);
-            if (requestRes.results)
+        if (text.length > 1) {
+            try {
+                const requestRes = await getGeocoding(text);
                 setError(false);
-            else {
+                setSearchResult(requestRes.results ?? []);
+            } catch {
+                setSearchResult(undefined);
+                setSearchError("Error on request geocoding");
                 setError(true);
-                setSearchError("No results found");
             }
-            setSearchResult(requestRes.results ?? []);
-        } catch {
-            setSearchResult([]);
-            setSearchError("Error on request geocoding");
-            setError(true);
-        }
+        } else
+            setSearchResult(undefined);
     }
 
     useEffect(() => {
@@ -79,13 +78,10 @@ export default function TopBar() {
     return (<View className="relative space-y-4 px-4 py-3">
         <View className="flex flex-row gap-3">
             <View className="flex-1 flex-row items-center px-3 bg-gray-300 rounded-full">
-                <Pressable onPress={() => {
-                    iptRef.current?.focus();
-                    handleChangeText(search);
-                }}>
+                <Pressable onPress={() => iptRef.current?.focus()}>
                     <Ionicons name="search" size={25} color="rgb(0, 122, 255)"/>
                 </Pressable>
-                <TextInput ref={iptRef} value={search} onChangeText={handleChangeText} placeholder="Search location..." className="flex-1 px-3 py-2 focus:outline-none"/>
+                <TextInput ref={iptRef} onFocus={() => handleChangeText(search)} value={search} onChangeText={handleChangeText} placeholder="Search location..." className="flex-1 px-3 py-2 focus:outline-none"/>
             </View>
 
             <Pressable onPress={getLocation} className="p-3 rounded-full">
@@ -94,22 +90,17 @@ export default function TopBar() {
         </View>
 
         {
-            searchResult && searchResult.length > 0 &&
-            <View className="absolute top-[80%] w-full bg-white z-10" style={{backgroundColor: "rgb(242, 242, 242)"}}>
-                {searchResult.map((item: iGeocoding, key: number) => (
-                    <Pressable key={key} className="flex-row border-b border-gray-300 px-2 py-6" onPress={() => {
-                        makeWeatherRequest(item.latitude, item.longitude, {
-                            city: item.name,
-                            region: item.admin1,
-                            country: item.country
-                        }).then(() => {
-                            setSearchResult([]);
-                        });
-                    }}>
-                        <Text className="flex-1"><Text className="font-bold">{item.name}</Text> {item.admin1}, {item.country}</Text>
-                    </Pressable>
-                ))}
-            </View>
+            searchResult && (<View className="absolute top-[80%] left-0 w-full bg-white z-10 shadow" style={{backgroundColor: "rgb(242, 242, 242)"}}>
+                {
+                    searchResult.length > 0 ?
+                        searchResult.map((item: iGeocoding, key: number) => (
+                            <Pressable key={key} className="flex-row border-b border-gray-300 px-2 py-6" onPress={() => makeWeatherRequest(item.latitude, item.longitude, {city: item.name, region: item.admin1, country: item.country})}>
+                                <Text className="flex-1"><Text className="font-bold">{item.name}</Text> {item.admin1}, {item.country}</Text>
+                            </Pressable>
+                        )) :
+                        <Text className="text-center py-6">No results found.</Text>
+                }
+            </View>)
         }
     </View>);
 }
