c489737d5513f034a64f5850287b9611602ca3e4
fguirama <florian.guiramand@protonmail.com>
2026-07-27 20:09:24 +0200

    fix: loading

--- 5-Manage-data-display/mobileModule05/src/component/Loading.tsx
+++ 5-Manage-data-display/mobileModule05/src/component/Loading.tsx
@@ -1,7 +1,7 @@
 import {ActivityIndicator, View} from "react-native";
 
 export default function Loading() {
-    return (<View className="flex-1 items-center justify-center bg-red-500">
+    return (<View className="flex-1 items-center justify-center">
         <ActivityIndicator size="large"/>
     </View>);
 }
